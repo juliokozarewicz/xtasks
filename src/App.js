992050bf6854e84f24e8da161183346f2dbe2759
JULIO KOZAREWICZ <julioerk@outlook.com>
2024-01-06 09:57:22 -0300

    [Build] Application structure update.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import HomeScreen from './1_screens/1_HomeScreen';
 
-// database
-import * as DataBase from './1_screens/4_database/DatabaseFunctions';
-
 
 const App = () => {
-
-  // create db if not exists
-  DataBase.openDatabaseAndCreateTable();
 
   return (
     <HomeScreen />
