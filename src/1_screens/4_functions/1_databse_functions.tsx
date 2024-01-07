aadf9691f10b8846de01c5baa485bee0ecbd92ea
JULIO KOZAREWICZ <julioerk@outlook.com>
2024-01-07 12:29:06 -0300

    [Build] CRUD database init.

---
+++ src/1_screens/4_functions/1_databse_functions.tsx
@@ -0,0 +1,65 @@
+import SQLite from 'react-native-sqlite-storage';
+
+// config db local
+// -------------------------------------------------------
+const db_name = 'tarefas';
+const table_name = 'lista_tarefas';
+const column_name_1 = 'tarefa';
+// -------------------------------------------------------
+
+
+// db connection
+// -------------------------------------------------------
+const db = SQLite.openDatabase({ name: `${db_name}.db`, location: 'default' });
+// -------------------------------------------------------
+
+
+// create database, table and column
+// -------------------------------------------------------
+export const openDatabaseAndCreateTable = () => {
+  db.transaction(
+    (tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS ${table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT, ${column_name_1} TEXT NOT NULL)`,
+      );
+    }
+  );
+};
+// -------------------------------------------------------
+
+
+// read data from db
+// -------------------------------------------------------
+export const ReadDataBase = (callback) => {
+    db.transaction(tx => {
+    tx.executeSql(
+        `SELECT ${column_name_1} FROM ${table_name}`,
+        [],
+        (results) => {
+            const data = [];
+            
+            for (let i = 0; i < results.rows.length; i++) {
+                data.push(results.rows.item(i)[column_name_1]);
+            }
+
+            callback(data);
+        }
+    );
+    });
+};
+// -------------------------------------------------------
+
+
+// create item in db
+// -------------------------------------------------------
+export const CreateItemDataBase = (task) => {
+
+    db.transaction((tx) => {
+        tx.executeSql(
+        `INSERT INTO ${table_name} (${column_name_1}) VALUES (?)`,
+        [`${task}`],
+        );
+    });
+    
+};
+// -------------------------------------------------------
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import HomeScreen from './1_screens/1_HomeScreen';
 
+// database
+import * as DataBase from './1_screens/4_functions/1_databse_functions';
+
 
 const App = () => {
+
+  // create db if not exists
+  DataBase.openDatabaseAndCreateTable();
+  
+  DataBase.ReadDataBase(result => {
+    console.log(result);
+  });
+
+  DataBase.ReadDataBase();
 
   return (
     <HomeScreen />
